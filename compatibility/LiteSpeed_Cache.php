c4d4f061576c0ba75e549356c2a8a11cc2b63ed1
Subrata Sarkar <ssarkar@emfluence.com>
2018-10-09 15:27:30 +0530

    File name: LiteSpeed_Cache.php Description: Comments are placed inside the file. Coding Standard used: WordPress-Extra

--- compatibility/LiteSpeed_Cache.php
+++ compatibility/LiteSpeed_Cache.php
@@ -1,32 +1,35 @@
 <?php
 
 
-    class PTO_LiteSpeed_Cache
-        {
-                        
-            function __construct()
-                {
-                    
-                    if( !is_plugin_active( 'litespeed-cache/litespeed-cache.php' ))
-                        return false;
-                    
-                    add_action( 'PTO/order_update_complete', array( $this, 'order_update_complete') );
-                }
-                
-                
-            function order_update_complete()
-                {
-                    
-                    if( method_exists( 'LiteSpeed_Cache_API', 'purge_all' ) ) 
-                        {
-                            LiteSpeed_Cache_API::purge_all() ;
-                        }
-                
-                }                        
-                                
-        }
-        
-    new PTO_LiteSpeed_Cache();
+class PTO_LiteSpeed_Cache {
 
+	/**
+	 * PTO_LiteSpeed_Cache constructor.
+	 * Description: phpcs error -> function visibility was not defined.
+	 */
+	public function __construct() {
+		/**
+		 * phpcs error: return statement is missing.
+		 * since action could only be added if is_plugin_active is true
+		 * we do not need to check an ! is_plugin_active() and return false from there
+		 * instead we can simply add_action if and only if is_plugin_active check returns true.
+		 * this prevents returning the boolean value from the function.
+		 */
 
-?>
+		if ( is_plugin_active( 'litespeed-cache/litespeed-cache.php' ) ) {
+			add_action( 'PTO/order_update_complete', array( $this, 'order_update_complete' ) );
+		}
+	}
+
+	/**
+	 * phpcs error -> function visibility was not defined.
+	 * Added public
+	 */
+	public function order_update_complete() {
+		if ( method_exists( 'LiteSpeed_Cache_API', 'purge_all' ) ) {
+			LiteSpeed_Cache_API::purge_all();
+		}
+	}
+}
+
+new PTO_LiteSpeed_Cache();
